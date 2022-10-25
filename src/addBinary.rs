09a1da4479a8fea500d1be9452610c0a71fc7d87
Ivan Polanis <polanisivan@gmail.com>
2022-10-25 13:55:40 -0300

    remove_element solved

---
+++ src/addBinary.rs
@@ -0,0 +1,10 @@
+impl Solution {
+  pub fn add_binary(a: String, b: String) -> String {
+      let mut lena = a.len();
+      let mut lenb = b.len();
+      let mut carry:bool = false;
+      while lena != 0 | lenb !=0 | !carry {
+        
+      }
+  }
+}
--- src/removeElement.rs
+++ src/removeElement.rs
@@ -1,27 +1,12 @@
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        if nums.len() == 0 {
-            return 0;
-        };
-        if nums.len() == 1 && nums[0] == val {
-            return 0;
-        }
-
-        let mut right: usize = nums.len() - 1;
-        let mut left = 0;
-        while left < right {
-            let num = nums[right];
-            if nums[left] == val && nums[left] != num {
-                nums[right] = nums[left];
-                nums[left] = num;
-                left += 1;
-                right -= 1;
-            } else if nums[left] == num {
-                right -= 1;
-            } else {
-                left += 1;
+        let mut act:usize = 0;
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums[act] = nums[i];
+                curr +=1;
             }
         }
-        return (right + 1) as i32;
+        return curr as i32;
     }
 }
