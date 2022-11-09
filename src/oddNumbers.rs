812101cb52e9708f2925099c1ade40e9fe8f4913
Polanis Ivan <polanisivan@gmail.com>
2022-11-09 19:09:13 -0300

    odd number 1.9ms

--- src/oddNumbers.rs
+++ src/oddNumbers.rs
@@ -1,11 +1,5 @@
 impl Solution {
   pub fn count_odds(low: i32, high: i32) -> i32 {
-      if((low + high) % 2 == 0 && ((low%2) != 0)){
-        return (high - low)/2 +1;
-      }
-      if ((low %2) == 0 && (high%2)!=0) || ((low %2) != 0 && (high%2)==0) {
-        return (high - low)/2 +1;
-      } 
-      return (high - low)/2;
+      return (high-low)/2 + (high % 2 | low % 2)
   }
 }
