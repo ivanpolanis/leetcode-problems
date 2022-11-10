bb0805c05d803b5280d575e1175dcb6406df167a
Polanis Ivan <polanisivan@gmail.com>
2022-11-10 11:05:37 -0300

    hamming weight solved

---
+++ src/hammingWeight.rs
@@ -0,0 +1,11 @@
+impl Solution {
+  pub fn hammingWeight (n: u32) -> i32 {
+    let mut count = 0;  
+    let mut number = 0;
+    for i in 0..32 {
+      number = n >> i;
+        if (number & 1) == 1 {count+=1}
+      }
+    return count;
+  }
+}
